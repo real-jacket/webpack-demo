a41a6b17a5796ed41b5009852a4acef970e3fd9f
kete <1762982273@qq.com>
2019-03-29 09:52:45 +0800

    HMR test

--- webpack-4/src/index.js
+++ webpack-4/src/index.js
@@ -3,11 +3,20 @@ require('./index.less');
 console.log('hello world');
 
 // eslint-disable-next-line no-undef
-if (!PRODUCTION) {
-  console.log('Debug info');
-}
+// if (!PRODUCTION) {
+//   console.log('Debug info');
+// }
 
-// eslint-disable-next-line no-undef
-if (PRODUCTION) {
-  console.log('Production log');
+// // eslint-disable-next-line no-undef
+// if (PRODUCTION) {
+console.log('Production log');
+// }
+
+document.querySelector('#demo').textContent = '这是一个测试';
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    // eslint-disable-next-line no-undef
+    printMe();
+  });
 }
